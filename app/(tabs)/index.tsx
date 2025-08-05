1aac5b1a7e2f411de22d941bed6573f329068053
fpena <felipe.pena1@accesscfa.com>
2025-08-04 20:27:34 -0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,233 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Controller, useForm } from "react-hook-form";
+import {
+  Button,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import {
+  KeyboardAwareScrollView,
+  KeyboardProvider,
+} from "react-native-keyboard-controller";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const windowHeight = Dimensions.get("window").height;
 
 export default function HomeScreen() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      phoneNumber: "",
+      address: "",
+    },
+  });
+  const onSubmit = (data: any) => {
+    trigger(undefined, { shouldFocus: true });
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <>
+      <KeyboardProvider>
+        <KeyboardAwareScrollView
+          contentContainerStyle={styles.container}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View>
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", marginBottom: 20 }}
+            >
+              Form
+            </Text>
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <View>
+                  <Text style={styles.label}>First name</Text>
+                  <TextInput
+                    placeholder="First name"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    style={styles.input}
+                    ref={ref}
+                  />
+                </View>
+              )}
+              name="firstName"
+            />
+            {errors.firstName && (
+              <Text style={styles.errorText}>This is required.</Text>
+            )}
+
+            <Controller
+              control={control}
+              rules={{
+                maxLength: 100,
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <View>
+                  <Text style={styles.label}>Last name</Text>
+                  <TextInput
+                    placeholder="Last name"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    style={styles.input}
+                    ref={ref}
+                  />
+                </View>
+              )}
+              name="lastName"
+            />
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <View>
+                  <Text style={styles.label}>Email</Text>
+                  <TextInput
+                    placeholder="Email"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    style={styles.input}
+                    ref={ref}
+                  />
+                </View>
+              )}
+              name="email"
+            />
+            {errors.email && (
+              <Text style={styles.errorText}>This is required.</Text>
+            )}
+
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <View>
+                  <Text style={styles.label}>Password</Text>
+                  <TextInput
+                    placeholder="Password"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    style={styles.input}
+                    ref={ref}
+                  />
+                </View>
+              )}
+              name="password"
+            />
+            {errors.password && (
+              <Text style={styles.errorText}>This is required.</Text>
+            )}
+
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <View>
+                  <Text style={styles.label}>Confirm Password</Text>
+                <TextInput
+                  placeholder="Confirm Password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                    style={styles.input}
+                    ref={ref}
+                  />
+                </View>
+              )}
+              name="confirmPassword"
+            />
+            {errors.confirmPassword && (
+              <Text style={styles.errorText}>This is required.</Text>
+            )}
+
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <View>
+                  <Text style={styles.label}>Phone Number</Text>
+                <TextInput
+                  placeholder="Phone Number"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  style={styles.input}
+                  />
+                </View>
+              )}
+              name="phoneNumber"
+            />
+            {errors.phoneNumber && (
+              <Text style={styles.errorText}>This is required.</Text>
+            )}
+
+            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+          </View>
+        </KeyboardAwareScrollView>
+      </KeyboardProvider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  container: {
+    gap: 20,
+    minHeight: windowHeight,
+    paddingHorizontal: 20,
+    paddingTop: 60,
+  },
+  errorText: {
+    marginBottom: 20,
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 20,
+    marginBottom: 30,
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
